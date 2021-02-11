26cc567d32ace131ca1f17d8167198051b41fbf8
Neil Oribello <neil.oribello@gmail.com>
2021-02-11 01:58:53 +0000

    wordseach with Nolan done

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+// PAIR PROGRAMMING - @Nolan-E & @neoribello
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+  
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    const column = letters[0].length;
+    const row = letters.length;
+  
+    let output = '';
+  
+    for (let i = 0; i < column; i++) {
+      for (let j = 0; j < row; j++) {
+        output += letters[j][i];
+      }
+      if (output.includes(word)) {
+        return true;
+      }
+      output = '';
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
